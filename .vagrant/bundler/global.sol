2f54127edc172fd8a9213fb8318c824c62445fef
Mark van der Zande <mark.vanderzande@kpn.com>
2023-06-01 22:02:32 +0200

    Initial commit

---
+++ .vagrant/bundler/global.sol
@@ -0,0 +1,1 @@
+{"dependencies":[["vagrant-registration",["= 1.3.4"]],["log4r",[">= 0"]],["concurrent-ruby",["~> 1.0"]],["i18n",[">= 0"]],["micromachine",[">= 2","< 4"]],["vagrant-vbguest",["= 0.31.0"]]],"checksum":"9b6d30bd92ffaf7be9240a27a80ed2ba8892cf3f1c689c702e104b7c68f862df","vagrant_version":"2.3.2"}
